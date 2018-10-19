aeead718ac313d8f18b615c3445d120a72d3f0c8
Franklin Bado <fbado66@gmail.com>
2018-10-19 14:51:34 -0400

    adding html to bill-page

--- my_billing.js
+++ my_billing.js
@@ -2,16 +2,16 @@
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementById("container8");
+var navbar = document.getElementById("botton_nav");
 
 // Get the offset position of the navbar
-var sticky = container8.offsetTop;
+var sticky = botton_nav.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    container8.classList.add("sticky")
+    botton_nav.classList.add("sticky")
   } else {
-    container8.classList.remove("sticky");
+    botton_nav.classList.remove("sticky");
   }
 }
