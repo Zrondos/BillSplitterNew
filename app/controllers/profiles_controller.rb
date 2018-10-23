b9b6ad65b8f6dc9814c0e5b0a3ce17d4dee5368c
Franklin Bado <fbado66@gmail.com>
2018-10-22 21:09:39 -0400

    making one more push

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,15 +10,8 @@ class ProfilesController < ApplicationController
     @users_bill = UsersBill.find(params[:id])
     @users = User.all.map{ |c| [ c.id] }
     @group_bill = @bill.group_bill
-<<<<<<< HEAD
-    @items=@bill.items
-  
-
-    
 
 
-=======
->>>>>>> master
   end
 
   def edit
